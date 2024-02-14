87312e5d280e3f690bcea327391bcfb59d9cf403
DanialPersian <146435665+DanialPersian@users.noreply.github.com>
2024-02-14 20:50:17 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Pokemon_Challange;
+
+//(string name, string type, int hitpoints, string weakness)
+Pokemon pikachu = new Pokemon("pikachu ","electric",60,"Fighting");
+
+Pokemon squirtle = new Pokemon("squirtle ", "water", 70, "Electric");
+
+Pokemon Charmander = new Pokemon("Charmander ", "fire ", 60, "water");
+
+Pokemon Bulbasaur = new Pokemon("Bulbasaur ", "Grass", 70, "Fire");
+
+Pokemon Rockruff = new Pokemon("Rockruff ", "Fighting", 60, "Grass");
+
+Console.WriteLine("which pokemon do you want to know about ");
+
+string name = Console.ReadLine();
+
+if  (name == "pikachu")
+{
+    Console.WriteLine(pikachu.name + " has a element of " + pikachu.type + " their power is " + pikachu.hitpoints + " the weakness of this pokemon is " + pikachu.weakness);
+}
+if (name == "squirtle")
+{
+    Console.WriteLine(squirtle.name + " has a element of " + squirtle.type + " their power is " + squirtle.hitpoints + " the weakness of this pokemon is " + squirtle.weakness);
+}
+if (name == "Charmander")
+{
+    Console.WriteLine(Charmander.name + " has a element of " + Charmander.type + " their power is " + Charmander.hitpoints + " the weakness of this pokemon is " + Charmander.weakness);
+}
+if (name == "Bulbasaur")
+{
+    Console.WriteLine(Bulbasaur.name + " has a element of " + Bulbasaur.type + " their power is " + Bulbasaur.hitpoints + " the weakness of this pokemon is " + Bulbasaur.weakness);
+}
+if (name == "Rockruff")
+{
+    Console.WriteLine(Rockruff.name + " has a element of " + Rockruff.type + " their power is " + Rockruff.hitpoints + " the weakness of this pokemon is " + Rockruff.weakness);
+}
---
+++ pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Challange
+{
+    internal class Pokemon
+    {
+        public string name;
+        public int hitpoints;
+        public string type;
+        public string weakness;
+
+        public Pokemon(string name, string type, int hitpoints, string weakness) : this(name, type,weakness)
+        {
+            this.hitpoints = hitpoints;
+        }
+        public Pokemon(string _name, string _type, string _weakness)
+        {
+            this.name = _name;
+            this.type = _type;
+            this.weakness = _weakness;
+        }
+    }
+}
